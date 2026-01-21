b92e28f4545c896dd35fb4d9e68d1143b910f2f2
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2026-01-21 17:12:12 +0100

    creating the attendance route for the controller

--- server/routes/api/card.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const cardController = require('../../controllers/attendanceControllers')
-
-// Route to verifiy UID of card
-router.post('/card', cardController.checkCard);
-module.exports = router;
---
+++ server/routes/attendance.js
@@ -0,0 +1,19 @@
+// On importe Express uniquement pour utiliser son système de routes
+const express = require("express");
+
+// On crée un routeur Express
+// Ce routeur va contenir les routes liées à la présence
+const router = express.Router();
+
+// On importe le controller
+// C’est lui qui contient la logique métier
+const attendanceController = require("../controllers/attendanceController");
+
+// Définition de la route POST /api/attendance
+// Quand une requête POST arrive sur cette URL,
+// la fonction handleAttendance est exécutée
+router.post("/attendance", attendanceController.handleAttendance);
+
+// On exporte le routeur
+// Pour pouvoir l’utiliser dans app.js
+module.exports = router;
